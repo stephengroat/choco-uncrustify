8714bc6754ed691bc6800ec998d2f9410b4bc420
Steve Wolfe <StevenWolfe@users.noreply.github.com>
2016-12-02 16:39:52 -0500

    fixed backwards checksums, cut back docs, removed comments

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,15 +1,10 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿
+$ErrorActionPreference = 'Stop';
 
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName= 'uncrustify' # arbitrary name for the package, used in messages
+$packageName= 'uncrustify'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://sourceforge.net/projects/uncrustify/files/uncrustify/uncrustify-0.64/uncrustify-0.64-win32.zip/download' # download url, HTTPS preferred
-$url64      = 'https://sourceforge.net/projects/uncrustify/files/uncrustify/uncrustify-0.64/uncrustify-0.64-win64.zip/download' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
-#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
+$url        = 'https://sourceforge.net/projects/uncrustify/files/uncrustify/uncrustify-0.64/uncrustify-0.64-win32.zip/download'
+$url64      = 'https://sourceforge.net/projects/uncrustify/files/uncrustify/uncrustify-0.64/uncrustify-0.64-win64.zip/download'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,34 +12,16 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   url64bit      = $url64
-  #file         = $fileLocation
 
-  softwareName  = 'uncrustify*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'uncrustify*'
 
-  # Checksums are required as of 0.10.0.
-  # e.g. checksum -t sha256 -f path\to\file
-  checksum      = '1dcddc67c87d50e58d0147a60b8b8b430ad2f360'
+  checksum      = '3a76aa568eb3c5fefeb9e726133c8c2c48d4e0f7'
   checksumType  = 'sha1'
-  checksum64    = '3a76aa568eb3c5fefeb9e726133c8c2c48d4e0f7'
+  checksum64    = '1dcddc67c87d50e58d0147a60b8b8b430ad2f360'
   checksumType64= 'sha1'
 
-  #MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
-  #OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s -u'        # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
-  #validExitCodes= @(0) #please insert other valid exit codes here
 }
 
 Install-ChocolateyZipPackage @packageArgs
