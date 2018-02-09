e25155b4d0464cb9348978c92c8de348e7226963
Stephen <stephengroat@users.noreply.github.com>
2018-02-08 17:10:11 -0800

    Update chocolateyuninstall.ps1

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -18,14 +18,8 @@ if ($key.Count -eq 1) {
     $file = "$($_.UninstallString)"
 
     if ($installerType -eq 'MSI') {
-      # The Product Code GUID is all that should be passed for MSI, and very
-      # FIRST, because it comes directly after /x, which is already set in the
-      # Uninstall-ChocolateyPackage msiargs (facepalm).
       $silentArgs = "$($_.PSChildName) $silentArgs"
 
-      # Don't pass anything for file, it is ignored for msi (facepalm number 2)
-      # Alternatively if you need to pass a path to an msi, determine that and
-      # use it instead of the above in silentArgs, still very first
       $file = ''
     }
 
